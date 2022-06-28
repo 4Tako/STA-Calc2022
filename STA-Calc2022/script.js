84b2a26eb036bea39468699ffef866fbd1117839
4Tako <cbkt1211014@gn.iwasaki.ac.jp>
2022-06-29 02:47:42 +0900

    <任意のコメント>

---
+++ STA-Calc2022/script.js
@@ -0,0 +1,24 @@
+function get_calc(btn){
+    if(btn.value == "="){
+        document.calculator.display.value = eval(document.calculator.display.value);
+    }else if(btn.value == "+/-"){
+        document.calculator.display.value = eval(document.calculator.display.value*-1);
+    }else if(btn.value == "√"){
+        document.calculator.display.value = eval(document.calculator.display.value**0.5);
+    }else if(btn.value == "π"){
+        document.calculator.display.value = eval(document.calculator.display.value*3.14);
+    }else if(btn.value == "%"){
+        document.calculator.display.value = eval(document.calculator.display.value*0.01);
+    }else if (btn.value == "AC"){
+        document.calculator.display.value = "";
+    }else{
+        if (btn.value == "×"){
+            btn.value = "*";
+    }else if (btn.value == "÷"){
+            btn.value = "/";
+        }
+        document.calculator.display.value += btn.value;
+        document.calculator.multi_btn.value = "×";
+        document.calculator.div_btn.value = "÷";
+    }
+}
